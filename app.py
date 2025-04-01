755c5c4b3483c0d6dce990853babc3b6c587cfe8
Forsakringstestet <thorstensson.felix@gmail.com>
2025-04-02 00:45:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-""import streamlit as st
+import streamlit as st
 from ai.openai_advisor import ask_openai, ask_openai_extract
 from recommender import generate_recommendation
 from export import export_excel, export_pdf, export_word
